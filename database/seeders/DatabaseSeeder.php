af7e94f17c5e336f22b78cc9ce94d31641cf5d95
Neil Froggatt <neilfr@live.com>
2023-01-26 17:25:36 -0500

    setup todos backend with sail

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Stage;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,13 +15,28 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+             'password' => Hash::make('password')
+         ]);
+
+         $stages=[
+             'To Do',
+             'In Progress',
+             'Done'
+         ];
+
+         collect($stages)->each(function ($stage){
+             Stage::factory()->create([
+                 'description' => $stage,
+             ]);
+         });
+
+
     }
 }
